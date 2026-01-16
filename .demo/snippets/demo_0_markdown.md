66aa7f242c5a7295a9840ded3e4bcecf391d1ed5
Barry Smart <barry.smart@endjin.com>
2026-01-16 16:35:45 +0000

    Refinements to demo.

--- .demo/snippets/demo_0_markdown.md
+++ .demo/snippets/demo_0_markdown.md
@@ -2,6 +2,5 @@ st.write("""
 In this talk, you are going to learn how to build a data app using [Streamlit](https://streamlit.io/), 
 a popular open-source framework for creating interactive web applications in Python.
 
-Hope to see you at the after party! 🎉
-
+We hope to inspire you to develop your own data apps! 🎉
 """)
--- app/first_app.py
+++ app/first_app.py
@@ -1,9 +1,14 @@
 # First Streamlit app!
+import streamlit as st
 
+st.title("Hello World!")
 
+st.write("""
+In this talk, you are going to learn how to build a data app using [Streamlit](https://streamlit.io/), 
+a popular open-source framework for creating interactive web applications in Python.
 
+We hope to inspire you to develop your own data apps! 🎉
+""")
 
-
-
-
+st.image("../assets/endjin-logo.svg")
 
